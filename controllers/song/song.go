ab5e830a7e186a7eed90928c7960671610e33fcd
Rahmat Kurniawan <koerniaone000@gmail.com>
2020-11-18 19:41:20 +0700

    update

---
+++ controllers/song/song.go
@@ -0,0 +1,23 @@
+package song
+
+import "log"
+
+func Create() {
+	log.Println("Create a Data")
+}
+
+func GetAll() {
+	log.Println("Get All Data")
+}
+
+func GetId() {
+	log.Println("Get Data")
+}
+
+func Update() {
+	log.Println("Update Data")
+}
+
+func Delete() {
+	log.Println("Delete Data")
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/rahmat-kurniawan/gin-app/app"
+)
+
+func main() {
+	app.Init()
+}
